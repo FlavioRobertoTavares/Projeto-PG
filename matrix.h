841233249c504a941b5228b3cd97fbbe56b5ba41
MusicalUsagi <tavaresbezerra4@gmail.com>
2024-06-06 19:09:45 -0300

    Correção na transformação do ponto

--- matrix.h
+++ matrix.h
@@ -8,21 +8,18 @@ using namespace std;
 
 class Matrix{
     public:
-        vector<vector<double>> elements;
-        vector<vector<double>> elements_inverse;
         Matrix(){
-            this->elements(4, vector<double>(4, 0))
-
-
+            this->elements = elements;
 
         }
-        
+        vector<vector<double>> elements((4, vector<double>(4, 0)));
+        vector<vector<double>> elements_inverse;
 
         ray transform_ray(const ray &r){
             double x, y, z;
-            x = r.origin.x*elements_inverse[0][0] + r.origin.y*elements_inverse[0][1] + r.origin.z*elements_inverse[0][2];
-            y = r.origin.x*elements_inverse[1][0] + r.origin.y*elements_inverse[1][1] + r.origin.z*elements_inverse[1][2];
-            z = r.origin.x*elements_inverse[2][0] + r.origin.y*elements_inverse[2][1] + r.origin.z*elements_inverse[2][2];
+            x = r.origin.x*elements_inverse[0][0] + r.origin.y*elements_inverse[0][1] + r.origin.z*elements_inverse[0][2] + elements_inverse[0][3];
+            y = r.origin.x*elements_inverse[1][0] + r.origin.y*elements_inverse[1][1] + r.origin.z*elements_inverse[1][2] + elements_inverse[1][3];
+            z = r.origin.x*elements_inverse[2][0] + r.origin.y*elements_inverse[2][1] + r.origin.z*elements_inverse[2][2] + elements_inverse[2][3];
             Point origin = Point(x, y, z);
 
             x = r.direction.x*elements_inverse[0][0] + r.direction.y*elements_inverse[0][1] + r.direction.z*elements_inverse[0][2];
